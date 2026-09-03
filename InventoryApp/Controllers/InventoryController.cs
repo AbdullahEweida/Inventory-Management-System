1774d25f41819ae062d88cdfac3e0c10df245944
ِAbdullahEweida <https://github.com/AbdullahEweida>
2026-09-03 10:39:10 +0300

    add Pagination in inventory

--- InventoryApp/Controllers/InventoryController.cs
+++ InventoryApp/Controllers/InventoryController.cs
@@ -1,6 +1,9 @@
 ﻿using InventoryApp.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace InventoryApp.Controllers
 {
@@ -13,21 +16,54 @@ namespace InventoryApp.Controllers
             _db = db;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString, int page = 1)
         {
-            var stock = await _db.Products
+            int pageSize = 10;
+            if (page < 1) page = 1;
+
+            var query = _db.Products.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                query = query.Where(p => p.Name.Contains(searchString) || p.SKU.Contains(searchString));
+            }
+
+            int totalProducts = await query.CountAsync();
+            int totalPages = (int)Math.Ceiling(totalProducts / (double)pageSize);
+
+            var stock = await query
                 .OrderBy(p => p.Name)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
                 .ToListAsync();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.SearchString = searchString;
+
+            ViewBag.LowStockCount = await _db.Products.CountAsync(p => p.StockQuantity <= p.LowStockThreshold);
 
             return View(stock);
         }
 
-        public async Task<IActionResult> LowStockAlerts()
+        public async Task<IActionResult> LowStockAlerts(int page = 1)
         {
-            var alerts = await _db.Products
-                .Where(p => p.StockQuantity <= p.LowStockThreshold)
+            int pageSize = 6;
+            if (page < 1) page = 1;
+
+            var query = _db.Products.Where(p => p.StockQuantity <= p.LowStockThreshold);
+
+            int totalAlerts = await query.CountAsync();
+            int totalPages = (int)Math.Ceiling(totalAlerts / (double)pageSize);
+
+            var alerts = await query
                 .OrderBy(p => p.StockQuantity)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
                 .ToListAsync();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
 
             return View(alerts);
         }
--- InventoryApp/obj/Debug/net10.0/InventoryApp.AssemblyInfo.cs
+++ InventoryApp/obj/Debug/net10.0/InventoryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5a0ebbe4ec5c03f949f2714fa2a9045754b880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f979e8cee42792246b4b35682d53c6ba23c585c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
